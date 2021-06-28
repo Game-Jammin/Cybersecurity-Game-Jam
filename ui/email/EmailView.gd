cee14ee01c0e3fa334c184df2fd854fb54bed4c8
Adam Fowler <ajohnfowler@gmail.com>
2021-06-27 21:44:50 -0300

    Selected backgrounds

--- ui/email/EmailView.gd
+++ ui/email/EmailView.gd
@@ -15,13 +15,15 @@ func load_email(email):
 
 func _process(delta):
 	$Options/Delete.disabled = selected.size() == 0
+	
+	update_selected_ui()
 
 func _on_From_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			if event.pressed:
 				if "from" in selected:
-					selected.remove("from")
+					selected.remove(selected.bsearch("from"))
 				else:
 					selected.append("from")
 				print (selected)
@@ -31,7 +33,14 @@ func _on_Subject_gui_input(event):
 		if event.button_index == BUTTON_LEFT:
 			if event.pressed:
 				if "subject" in selected:
-					selected.remove("subject")
+					selected.remove(selected.bsearch("subject"))
 				else:
 					selected.append("subject")
 				print (selected)
+
+func update_selected_ui():
+	# from selected
+	from_text.get_node("Select").visible = "from" in selected
+	
+	# subject selected
+	subject_text.get_node("Select").visible = "subject" in selected
