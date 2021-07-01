b3baaf89e7cce907502bef01cd09faaa4c507864
Adam Fowler <ajohnfowler@gmail.com>
2021-07-01 17:14:55 -0300

    Massive UI Update!

--- singletons/WindowManager.gd
+++ singletons/WindowManager.gd
@@ -5,6 +5,7 @@ onready var windows_node = get_node("/root/Screen/Windows")
 func open_window(window_name):
 	# return if window is already open
 	if windows_node.has_node(window_name + 'Window'):
+		set_focus(window_name + 'Window')
 		return
 	var new_window = load('res://ui/windows/' + window_name + '.tscn').instance()
 	windows_node.add_child(new_window)
