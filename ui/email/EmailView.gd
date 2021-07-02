4b26adb57b8efa0a5229a52948fbe36eb9fc616a
Adam Fowler <ajohnfowler@gmail.com>
2021-07-02 12:29:05 -0300

    Titlebar Scaling

--- ui/email/EmailView.gd
+++ ui/email/EmailView.gd
@@ -48,7 +48,7 @@ func fill_body_lines(body_lines):
 		line_number = line_number + 1
 
 func fill_email_attachments(attachments):
-	$EmailBodyBackground/EmailBody/Container/AttachmentHeader.visible = attachments.size() > 0
+	#$EmailBodyBackground/EmailBody/Container/AttachmentHeader.visible = attachments.size() > 0
 	
 	for attachment in attachments_container.get_children():
 		attachment.queue_free()
